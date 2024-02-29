88061bcc1a1138cd8a6b371150838855f2580852
João Vitor Mesquita de Lima <99664194+joaomesquitalima@users.noreply.github.com>
2024-02-29 09:45:39 -0400

    Update main.py

--- upload_arquivos/main.py
+++ upload_arquivos/main.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, request, send_from_directory, redirect
+from flask import Flask, render_template, request, send_from_directory, redirect, jsonify
 import os
-
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'arquivos'
 
 @app.route('/')
 def index():
     return render_template('index.html', os=os)
+
+
+@app.route('/delete/<filename>', methods=['DELETE'])
+def delete_file(filename):
+    try:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        os.remove(file_path)
+        return jsonify({'success': True})
+    except Exception as e:
+        print(f'Erro ao excluir o arquivo: {e}')
+        return jsonify({'success': False})
+
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
