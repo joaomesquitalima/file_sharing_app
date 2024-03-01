2d09634d3dec7ce1c2e9770d0f72501406154fdc
João Vitor Mesquita de Lima <99664194+joaomesquitalima@users.noreply.github.com>
2024-03-01 08:38:05 -0400

    Update main.py

--- upload_arquivos/main.py
+++ upload_arquivos/main.py
@@ -1,43 +1,48 @@
 from flask import Flask, render_template, request, send_from_directory, redirect, jsonify
 import os
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'arquivos'
 
-@app.route('/')
-def index():
-    return render_template('index.html', os=os)
+class MeuAppFlask:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.app.config['UPLOAD_FOLDER'] = 'arquivos'
+        
+        self.app.route('/')(self.index)
+        self.app.route('/delete/<filename>', methods=['DELETE'])(self.delete_file)
+        self.app.route('/upload', methods=['POST'])(self.upload_file)
+        self.app.route('/uploads/<filename>')(self.uploaded_file)
 
+    def index(self):
+        return render_template('index.html', os=os)
 
-@app.route('/delete/<filename>', methods=['DELETE'])
-def delete_file(filename):
-    try:
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        os.remove(file_path)
-        return jsonify({'success': True})
-    except Exception as e:
-        print(f'Erro ao excluir o arquivo: {e}')
-        return jsonify({'success': False})
+    def delete_file(self, filename):
+        try:
+            file_path = os.path.join(self.app.config['UPLOAD_FOLDER'], filename)
+            os.remove(file_path)
+            return jsonify({'success': True})
+        except Exception as e:
+            print(f'Erro ao excluir o arquivo: {e}')
+            return jsonify({'success': False})
 
+    def upload_file(self):
+        if 'file' not in request.files:
+            return redirect(request.url)
 
+        file = request.files['file']
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return redirect(request.url)
+        if file.filename == '':
+            return redirect(request.url)
 
-    file = request.files['file']
+        if file:
+            filename = os.path.join(self.app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+            return redirect('/')
 
-    if file.filename == '':
-        return redirect(request.url)
+    def uploaded_file(self, filename):
+        return send_from_directory(self.app.config['UPLOAD_FOLDER'], filename)
 
-    if file:
-        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filename)
-        return redirect('/')
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    def run(self):
+        self.app.run(debug=False, host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    meu_app = MeuAppFlask()
+    meu_app.run()
