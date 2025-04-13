662d40cdcd5114258303d3f96f539ffa11571bf2
João Vitor Mesquita de Lima <joaomesquitalima6@gmail.com>
2025-04-13 12:20:46 -0300

    bootstrap removido

--- upload_arquivos/main.py
+++ upload_arquivos/main.py
@@ -20,22 +20,8 @@ class MeuAppFlask:
         self.app.route('/uploads/<filename>',methods=['GET'])(self.uploaded_file)
         self.app.route('/shutdown')(self.shutdown)
         self.app.route('/')(self.index)
-        # self.app.route('/login',methods=['POST'])(self.login)
+        
         self.app.route('/arquivos', methods=['POST','GET'])(self.index)
-
-    # def home(self):
-    #     return render_template('home.html')
-    
-    # def login(self):
-    #     nome = request.form['nome']
-    #     senha = request.form['senha']
-
-    #     if nome == 'mesquita' and senha == '040586ac':
-    #         return redirect('/arquivos')
-
-
-    #     # if nome == 'mesquita' and senha == '040586ac':
-    #     #     return redirect('/arquivos')
 
     def index(self):
         files = os.listdir(self.app.config['UPLOAD_FOLDER'])
